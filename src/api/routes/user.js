00cb8b175535e315006b28dba0aec8947f0d4a83
Ana <102660581+Miterracita@users.noreply.github.com>
2024-08-20 09:06:36 +0200

    Add files via upload

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -1,10 +1,10 @@
-// const { isAuth } = require("../../middlewares/auth");
+const { isAuth, isAdmin } = require("../../middlewares/auth");
 const { registro, login, getUsers, getUserById } = require("../controllers/user");
 const userRoutes = require("express").Router();
 
 userRoutes.post("/registro", registro);
 userRoutes.post("/login", login);
-userRoutes.get("/allUsers", getUsers);
-userRoutes.get("/:id", getUserById);
+userRoutes.get("/allUsers", [isAdmin], getUsers);
+userRoutes.get("/:id", [isAuth], getUserById);
 
 module.exports = userRoutes;
